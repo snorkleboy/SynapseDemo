334f41932dbc6fab806a5e4535713c72c84d0b6d
Artem Kharshan <timkharshan@hotmail.com>
2018-03-13 20:49:27 -0700

    jbuilder of some users info

--- app/controllers/api/synapse_users_controller.rb
+++ app/controllers/api/synapse_users_controller.rb
@@ -1,7 +1,29 @@
+require 'json'
 class Api::SynapseUsersController < ApplicationController
     def index
         synapse_init
-        users = synapse_get_users
-        render json: {users: users.map{|users| users.legal_names}}
+        @users = synapse_get_users
+        # users = users.map{|user| extract_info(user)}
+        # p JSON.generate(users)
+        # render text: JSON.generate(users),content_type: 'application/json'
     end
+end
+
+
+def extract_info(user)
+    return {
+        logins:user.logins,
+        phone_numbers:user.phone_numbers,
+        legal_names:user.legal_names,
+        permission:user.permission,
+        note:user.note ,
+        supp_id:user.supp_id ,
+        is_business:user.is_business ,
+        cip_tag:user.cip_tag ,
+        flag:user.flag ,
+        ips:user.ips,
+        oauth_key:user.oauth_key,
+        expires_in:user.expires_in,
+        base_documents:user.base_documents
+    }
 end
---
+++ app/views/api/synapse_users/index.json.jbuilder
@@ -0,0 +1,2 @@
+
+json.users @users, :logins,:phone_numbers,:legal_names,:note,:is_business,:cip_tag,:flag
